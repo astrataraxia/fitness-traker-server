9cea4d27f9c1b90708863817c86732488049560d
stone <astrataraxia@gmail.com>
2024-07-27 20:09:16 +0900

    start project

---
+++ src/test/java/com/fitness/tracker_server/TrackerServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fitness.tracker_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrackerServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
